5d0b1d0903ca352326b73580cb6bc4e4869a6c08
Nabil Wadih <nabil.wadih@gmail.com>
2016-11-22 15:38:04 -0500

    unique usernames and emails

--- db/migrate/20160911214247_create_users.rb
+++ db/migrate/20160911214247_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username, :null => false
+      t.string :username, :null => false, :unique => true
       t.string :password_digest, :null => false
       t.string :first_name, :null => false
       t.string :last_name, :null => false
-      t.string :email, :null => false
+      t.string :email, :null => false, :unique => true
       t.date :date_of_birth, :null => false
 
 
