5302a99466bc8a6422140edb70a5091c34ed306b
Nabil <nabil.wadih@gmail.com>
2016-12-04 22:12:51 -0500

    fixed editing national survey

--- app/models/national_survey.rb
+++ app/models/national_survey.rb
@@ -1,5 +1,6 @@
 class NationalSurvey < ApplicationRecord
     belongs_to :user
-    validates :user_id, :jb_region, :jb_neighborhood, :num_chapters, :national_jb, :leadership_composition, :national_events, :num_active, :num_active_national, :gender_ratio, :S3Q1, :S3Q2, :S3Q3, :S3Q4, :S3Q5, :S3P1Q1, :S3P1Q2, :S3P1Q3, :S3P1Q4, :S3P1Q4, :S3P1Q5, :S3P1Q6, :S3P2Q1, :S3P2Q2, :S3P2Q3, :S3P2Q4, :S3P2Q5, :S4P1Q1, :S4P1Q2, :S4P2Q1, :S4P2Q2, :S4P3Q1, :S4P3Q2, :S4P3Q3, :S4P3Q4, :S4P4Q1, :S4P4Q2, :S4P4Q3, :S4P4Q4, :S5Q1, :S5Q2, :S6Q1, :S6Q2, :presence => true
+    validates :user_id, :jb_region, :jb_neighborhood, :num_chapters, :national_jb, :leadership_composition, :national_events, :num_active, :num_active_national, :gender_ratio, :S3Q5, :S3P2Q3, :S3P2Q4, :S3P2Q5, :S4P1Q2, :S4P4Q4, :S6Q1, :S6Q2, :presence => true
+    validates :S3Q1, :S3Q2, :S3Q4, :S3P1Q1, :S3P1Q2, :S3P1Q3, :S3P1Q4, :S3P1Q5, :S3P1Q6, :S3P2Q1, :S3P2Q2, :S4P2Q1, :S4P2Q2, :S4P3Q1, :S4P3Q2, :S4P3Q3, :S4P3Q4, :S4P4Q1, :S4P4Q2, :S4P4Q3, :S5Q1, :S5Q2, inclusion: { in: [ true, false ] }
 
 end
